8887f0be88dd98c390a7239cc95853e7469e619c
Illia Oliynik <illiaoliynik@gmail.com>
2020-04-23 19:46:13 +0300

    Updates

---
+++ precache-manifest.9a4c407e03beec1fa93834ab0746c7d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd15c30afaa18ab0201776f7409b4ab4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "26d1661a212078421fa6",
+    "url": "./static/js/2.b2110f2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b2110f2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19beaffe5e965cc9677b",
+    "url": "./static/js/main.7e0ef60c.chunk.js"
+  },
+  {
+    "revision": "9a40466c9e3fec559fa3",
+    "url": "./static/js/runtime-main.6a591e0b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.9a4c407e03beec1fa93834ab0746c7d6.js"
 );
 
 self.addEventListener('message', (event) => {
